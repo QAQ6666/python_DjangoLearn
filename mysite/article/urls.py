8be6111987d99b3d202c836759417d7ee274da4f
QAQ6666 <2606292175@qq.com>
2020-12-05 10:35:02 +0800

    buid delete and edit article Funtions

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.urls import path,re_path
-#from . import views, list_views
-from . import views
+from . import views, list_views
 
 app_name = "article"
 
@@ -10,6 +9,8 @@ urlpatterns = [
     path('rename-column/',views.rename_article_column,name='rename_article_column'),
     path('article-post/', views.article_post, name='article_post'),
     path('article-list/', views.article_list, name='article_list'),
+path('del-article/', views.del_article, name='del_article'),
+path('redit-article/<int:article_id>', views.redit_article, name='redit_article'),
     #url(r'^article-column/$', views.article_column, name="article_column"),
     # url(r'^rename-column/$', views.rename_article_column, name="rename_article_column"),
     # url(r'^del-column/$', views.del_article_column, name="del_article_column"),
@@ -18,6 +19,8 @@ urlpatterns = [
      re_path(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name="article_detail"),
     # url(r'^del-article/$', views.del_article, name="del_article"),
     # url(r'^redit-article/(?P<article_id>\d+)/$', views.redit_article, name="redit_article"),
+path('list-article-titles/', list_views.article_titles, name='article_titles'),
+path('article-content/<int:id>/<slug:slug>/', list_views.article_detail, name='article_content'),
     # url(r'^list-article-titles/$', list_views.article_titles, name="article_titles"),
     # url(r'^list-article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', list_views.article_detail, name="list_article_detail"),
     # url(r'^list-article-titles/(?P<username>[-\w]+)/$', list_views.article_titles, name="author_articles"),
